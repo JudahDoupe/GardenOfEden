5a1a4c8074fe5fd2d5de5b39fb87be463f00ee7c
Judah Doupe <judahdoupe@gmail.com>
2019-09-02 20:18:28 -0400

    clean up

--- Assets/Scripts/Environment/EnvironmentAPI.cs
+++ Assets/Scripts/Environment/EnvironmentAPI.cs
@@ -2,7 +2,9 @@
 
 public class EnvironmentAPI : MonoBehaviour
 {
-    private static EnvironmentAPI Instance;
+    public static EnvironmentAPI Instance;
+
+    public bool ShowVoxels = false;
 
     private VoxelService _voxelService;
 
@@ -43,5 +45,10 @@ public class EnvironmentAPI : MonoBehaviour
         var coord = new VoxelCoord(location);
         var amount = Instance._voxelService.RemoveSoil(coord, requestedAmount);
         return amount;
+    }
+    public static void AddSoil(Vector3 location, float amount)
+    {
+        var coord = new VoxelCoord(location);
+        Instance._voxelService.AddSoil(coord, amount);
     }
 }
