da3e7a25c028ed237c1fbc12c79a370c2da7ad3d
jjdoupe <jjdoupe@sep.com>
2020-01-24 20:03:58 -0500

    [4hr] added uiElements runtime

---
+++ Assets/Scripts/GamePlay/UI/EvolutionUIController.cs
@@ -0,0 +1,23 @@
+﻿using Unity.UIElements.Runtime;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EvolutionUIController : MonoBehaviour
+{
+    private VisualElement _evolutionUi;
+    void Start()
+    {
+        _evolutionUi = GetComponent<PanelRenderer>().visualTree;
+        var leavesButton = _evolutionUi.Q<Button>("leaves");
+        if (leavesButton != null)
+        {
+            leavesButton.clickable.clicked += () => { Debug.Log("Clicked the leaves button"); };
+        }
+        else
+        {
+            Debug.Log("Could not find leaves button");
+        }
+    }
+
+}
