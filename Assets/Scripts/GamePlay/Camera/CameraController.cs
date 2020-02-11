6d447e39d61867a94542617b442c791bf9800b93
jjdoupe <jjdoupe@sep.com>
2020-02-11 08:16:45 -0500

    [0.5hr] finished camera state machine

--- Assets/Scripts/GamePlay/Camera/CameraController.cs
+++ Assets/Scripts/GamePlay/Camera/CameraController.cs
@@ -1,79 +1,56 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public enum State
+    {
+        Cinematic,
+        Birdseye
+    }
 
-    public CameraMode CurrentMode = CameraMode.Cinematic;
+    public void SetState(State state)
+    {
+        _currentState = _states[state];
+        _currentState.Transition();
+    }
 
     /* INNER MECHINATIONS */
 
     private CameraTransform _transform;
     private CameraFocus _focus;
 
-    public enum CameraMode
-    {
-        Cinematic,
-        BirdsEye,
-    }
+    private Dictionary<State, CameraState> _states;
+    private CameraState _currentState;
 
     private void Start()
     {
         _focus = FindObjectOfType<CameraFocus>();
         _transform = FindObjectOfType<CameraTransform>();
+        _states = new Dictionary<State, CameraState>
+        {
+            {State.Cinematic, new Cinematic(_transform, _focus)},
+            {State.Birdseye, new BirdsEye(_transform, _focus)},
+        };
+
+        SetState(State.Cinematic);
     }
-    private void LateUpdate()
+
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.C))
         {
-            CurrentMode = CurrentMode == CameraMode.Cinematic ? CameraMode.BirdsEye : CameraMode.Cinematic;
+            SetState(State.Cinematic);
         }
-
-        switch (CurrentMode)
+        else if (Input.GetKeyDown(KeyCode.B))
         {
-            case CameraMode.Cinematic:
-                UpdateCinematic();
-                break;
-            case CameraMode.BirdsEye:
-                UpdateBirdsEye();
-                break;
+            SetState(State.Birdseye);
         }
     }
 
-    private void UpdateCinematic()
+    private void LateUpdate()
     {
-        _transform.MoveSpeed = 0.25f;
-        _transform.LookSpeed = 0.75f;
-        _focus.PrimaryFocus.IsDrifting = true;
-        _focus.SecondaryFocus.IsDrifting = true;
-
-        if (_focus.PrimaryFocus.Object != null)
-        {
-            var focusBounds = _focus.PrimaryFocus.Object.GetBounds();
-            var pDistance = Mathf.Max(focusBounds.extents.x, focusBounds.extents.y, focusBounds.extents.z) * (145f / Camera.main.fieldOfView);
-            var pPosition = _focus.PrimaryFocus.Position(pDistance);
-            var direction = (transform.position - pPosition).normalized;
-
-            if (_focus.SecondaryFocus.Object != null)
-            {
-                var sDistance = Vector3.Distance(_focus.PrimaryFocus.Object.transform.position, _focus.SecondaryFocus.Object.transform.position);
-                var sPosition = _focus.SecondaryFocus?.Position(sDistance) ?? pPosition;
-                direction = (pPosition - sPosition).normalized;
-            }
-
-            _transform.TargetPosition = pPosition + (direction * pDistance);
-            _transform.TargetFocusPosition = pPosition;
-        }
+        _currentState.Update();
     }
-
-    private void UpdateBirdsEye()
-    {
-        _transform.MoveSpeed = 1f; //These values should only be set on transistion
-        _transform.LookSpeed = 10;
-        _focus.PrimaryFocus.IsDrifting = false;
-        _focus.SecondaryFocus.IsDrifting = false;
-
-        _transform.TargetPosition = new Vector3(0, 350, -100);
-        _transform.TargetFocusPosition = _focus.PrimaryFocus.Position(0);
-    }
-
 }
