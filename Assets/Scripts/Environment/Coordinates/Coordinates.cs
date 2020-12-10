0154876274d63eb1b7ec8225377fe896b982ac66
jjdoupe <jjdoupe@sep.com>
2020-12-10 13:44:22 -0500

    [2hr] WIP started spherical land renderer

--- Assets/Scripts/Environment/Coordinates/Coordinates.cs
+++ Assets/Scripts/Environment/Coordinates/Coordinates.cs
@@ -3,34 +3,34 @@ using UnityEngine;
 
 namespace Assets.Scripts.Environment.Coordinates
 {
-    struct CartesianCoord
+    public struct CartesianCoord
     {
-        public float X;
-        public float Y;
-        public float Z;
+        public float3 XYZ;
+        public float X => XYZ.x;
+        public float Y => XYZ.y;
+        public float Z => XYZ.z;
 
         public CartesianCoord(float x, float y, float z)
         {
-            X = x;
-            Y = y;
-            Z = z;
+            XYZ = new float3(x, y, z);
         }
-
-        public CartesianCoord(Vector3 v)
+        public CartesianCoord(float3 v)
         {
-            X = v.x;
-            Y = v.y;
-            Z = v.z;
+            XYZ = v;
         }
-
         public CartesianCoord(SphericalCoord sc)
         {
-            X = math.cos(sc.Phi) * math.cos(sc.Theta) * sc.Height;
-            Y = math.sin(sc.Phi) * sc.Height;
-            Z = math.cos(sc.Phi) * math.sin(sc.Theta) * sc.Height;
+            XYZ = new float3(
+                math.cos(sc.Phi) * math.cos(sc.Theta) * sc.Height,
+                math.sin(sc.Phi) * sc.Height,
+                math.cos(sc.Phi) * math.sin(sc.Theta) * sc.Height);
         }
+        
+        public SphericalCoord ToSpherical() => new SphericalCoord(this);
+        public CubeMapCoord ToCubeMap() => new CubeMapCoord(this);
     }
-    struct SphericalCoord
+
+    public struct SphericalCoord
     {
         public float Theta;
         public float Phi;
@@ -42,12 +42,94 @@ namespace Assets.Scripts.Environment.Coordinates
             Phi = phi;
             Height = height;
         }
-
         public SphericalCoord(CartesianCoord cc)
         {
             Height = math.sqrt(math.pow(cc.X, 2) + math.pow(cc.Y, 2) + math.pow(cc.Z, 2));
             Theta = math.acos(cc.Z / Height);
             Phi = math.atan(cc.Y / cc.X);
+        }
+
+        public CartesianCoord ToCartesian() => new CartesianCoord(this);
+        public CubeMapCoord ToCubeMap() => new CubeMapCoord(ToCartesian());
+    }
+
+    public struct CubeMapCoord
+    {
+        public CubemapFace Face;
+
+        public float2 UV;
+        public float U => UV.x;
+        public float V => UV.y;
+
+        public int2 XY => math.int2(math.floor(UV * EnvironmentalChunkService.TextureSize));
+        public int X => XY.x;
+        public int Y => XY.y;
+
+        public CubeMapCoord(float u, float v, CubemapFace face)
+        {
+            UV = new float2(u, v);
+            Face = face;
+        }
+        public CubeMapCoord(int x, int y, CubemapFace face)
+        {
+            UV = new float2(x, y) / EnvironmentalChunkService.TextureSize;
+            Face = face;
+        }
+        public CubeMapCoord(CartesianCoord cc)
+        {
+            var v = cc.XYZ;
+            var abs = math.abs(v);
+
+            int greatestIndex = 0;
+            for (int i = 1; i < 3; i++)
+                if (abs[i] > abs[greatestIndex])
+                    greatestIndex = i;
+
+            v /= abs[greatestIndex];
+
+            switch (greatestIndex)
+            {
+                case 0:
+                    if (v.x > 0)
+                    {
+                        Face = CubemapFace.PositiveX;
+                        UV = new float2(v.z, v.y);
+                    }
+                    else
+                    {
+                        Face = CubemapFace.NegativeX;
+                        UV = new float2(-v.z, v.y);
+                    }
+                    break;
+                case 1:
+                    if (v.y > 0)
+                    {
+                        Face = CubemapFace.PositiveY;
+                        UV = new float2(-v.x, v.z);
+                    }
+                    else
+                    {
+                        Face = CubemapFace.NegativeY;
+                        UV = new float2(-v.x, -v.z);
+                    }
+                    break;
+                case 2:
+                    if (v.z > 0)
+                    {
+                        Face = CubemapFace.PositiveZ;
+                        UV = new float2(-v.x, v.y);
+                    }
+                    else
+                    {
+                        Face = CubemapFace.NegativeZ;
+                        UV = new float2(v.x, v.y);
+                    }
+                    break;
+                default:
+                    Face = CubemapFace.PositiveX;
+                    UV = new float2(v.z, v.y);
+                    break;
+            }
         }
     }
 }
--- Assets/Scripts/Environment/Land/SphereLand.shader
+++ Assets/Scripts/Environment/Land/SphereLand.shader
@@ -11,6 +11,9 @@ Shader "Custom/SphereLand"
 
 		_BedRockColor("Bedrock Color", color) = (1,1,1,0)
 	    _SoilColor("Live Soil Color", color) = (1,1,1,0)
+
+		_EdgeLength("Tesselation Edge length", Range(2,50)) = 15
+
         _TopographyFrequency ("Topography Line Frequency", Range(5,50)) = 10
         _TopographyWidth ("Topography Line Width", Range(0,1)) = 0.5
         _TopographyDarkening ("Topography Line Darkening", Range(0,0.5)) = 0.2
@@ -26,40 +29,74 @@ Shader "Custom/SphereLand"
 		#include "Tessellation.cginc"
 		#include "UnityShaderVariables.cginc"
 
+        sampler2D _LandMapXPos;
+        sampler2D _LandMapXNeg;
+        sampler2D _LandMapYPos;
+        sampler2D _LandMapYNeg;
+        sampler2D _LandMapZPos;
+        sampler2D _LandMapZNeg;
 
+        half _TopographyWidth;
+        half _TopographyFrequency;
+		half _TopographyDarkening;
+		float _EdgeLength;
+		float Epsilon = 1e-10;
 
+		struct appdata 
+		{
+			float4 vertex : POSITION;
+			float4 tangent : TANGENT;
+			float3 normal : NORMAL;
+			float2 texcoord : TEXCOORD0;
+			float4 color : COLOR;
+		};
 
+		struct Input 
+		{
+			float2 uv_LandMap : TEXCOORD0;
+			float4 screenPos : TEXCOORD1;
+			float4 color : COLOR;
+		};
 
+        float3 FindNormal(float4 uv, sampler2D map, int channel)
+		{
+			float4 h;
+			float u = 1.0 / 512.0;
+			h[0] = tex2Dlod(map, uv + float4(u * float2(0, -1), 0, 0))[channel];
+			h[1] = tex2Dlod(map, uv + float4(u * float2(-1, 0), 0, 0))[channel];
+			h[2] = tex2Dlod(map, uv + float4(u * float2(1, 0), 0, 0))[channel];
+			h[3] = tex2Dlod(map, uv + float4(u * float2(0, 1), 0, 0))[channel];
+			float3 n;
+			n.z = h[0] - h[3];
+			n.x = h[1] - h[2];
+			n.y = 2;
+			return normalize(n);
+		}
+        
+        float4 tess(appdata v0, appdata v1, appdata v2)
+		{
+			return UnityEdgeLengthBasedTess(v0.vertex, v1.vertex, v2.vertex, _EdgeLength);
+		} 
 
+		void disp(inout appdata v)
+		{	
+			sampler2D map = _LandMapXPos;
+			float4 soil = tex2Dlod(map, float4(v.texcoord, 0, 0));
+			int channel = 3;
 
+			float3 textureNormal = FindNormal(float4(v.texcoord,0,0), map, channel);
 
-        sampler2D _MainTex;
-
-        struct Input
-        {
-            float2 uv_MainTex;
-        };
-
-        half _Glossiness;
-        half _Metallic;
-        fixed4 _Color;
-
-        // Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
-        // See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
-        // #pragma instancing_options assumeuniformscaling
-        UNITY_INSTANCING_BUFFER_START(Props)
-            // put more per-instance properties here
-        UNITY_INSTANCING_BUFFER_END(Props)
+			v.vertex += float4(v.normal * soil[channel], 0);
+			v.normal = normalize(textureNormal.x * v.tangent + textureNormal.y * v.normal + textureNormal.z * cross(v.tangent, v.normal));
+			v.color = float4(0,0,0,0);
+		}
 
         void surf (Input IN, inout SurfaceOutputStandard o)
         {
-            // Albedo comes from a texture tinted by color
-            fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
-            o.Albedo = c.rgb;
-            // Metallic and smoothness come from slider variables
-            o.Metallic = _Metallic;
-            o.Smoothness = _Glossiness;
-            o.Alpha = c.a;
+            o.Albedo = 0.5;
+            o.Metallic = 0;
+            o.Smoothness = 0;
+            o.Alpha = 0;
         }
         ENDCG
     }
