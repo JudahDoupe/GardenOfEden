dd29003cf84460f8e585c9381f23dcdc8b115c87
Judah Doupe <jjdoupe@sep.com>
2022-02-17 09:13:34 -0500

    [1.5hr] continued work on tectonics exhibit

---
+++ Assets/Exhibits/HeightMapVisualizer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeightMapVisualizer : MonoBehaviour
+{
+    public GameObject[] Planes = new GameObject[6];
+    public GameObject Shpere;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
