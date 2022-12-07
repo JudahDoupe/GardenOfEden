4749bb48a56f055d4a679c1ba49f2fe81abd32f2
judah <judahdoupe@gmail.com>
2022-12-07 18:38:17 -0500

    [1.5hr] WIP added UI to scene

---
+++ Assets/_Gameplay/UI/Toolbar/ToolbarController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class ToolbarController : MonoBehaviour
+{
+    public void ShowToolbar()
+    {
+        
+    }
+    
+    public void HideToolbar()
+    {
+        
+    }
+    
+    public void SelectGlobe()
+    {
+        
+    }
+
+    public void SelectLandSystem()
+    {
+        
+    }
+
+    public void SelectMovePlateTool()
+    {
+        
+    }
+}
