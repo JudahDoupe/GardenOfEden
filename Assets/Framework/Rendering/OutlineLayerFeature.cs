a8f630c88ad1ce61010c3d2e7bc4476fb2c3ad74
judahdoupe <judahdoupe@gmail.com>
2023-02-07 20:59:43 -0500

    [2hr] WIP the continetal replacement shader only works if it is already the current shader on the material

--- Assets/Framework/Rendering/OutlineLayerFeature.cs
+++ Assets/Framework/Rendering/OutlineLayerFeature.cs
@@ -37,7 +37,8 @@ public class OutlineLayerFeature : ScriptableRendererFeature
         private string _layerName = "OutlinedGroup";
         private Material _replacementMaterial;
         private Material _outlineMaterial;
-        private RTHandle _tmpHandle;
+        private RTHandle _outlineHandle;
+        private RTHandle _sourceHandle;
 
         public void Setup(in RenderingData renderingData, string layerName, Material replacementMaterial, Material outlineMaterial)
         {
@@ -46,7 +47,8 @@ public class OutlineLayerFeature : ScriptableRendererFeature
             _outlineMaterial = outlineMaterial;
             var desc = renderingData.cameraData.cameraTargetDescriptor;
             desc.depthBufferBits = (int) DepthBits.None;
-            RenderingUtils.ReAllocateIfNeeded(ref _tmpHandle, desc, name: "_TempOutlineBlitMaterialTexture");
+            RenderingUtils.ReAllocateIfNeeded(ref _outlineHandle, desc, name: "_TempOutlineTexture");
+            RenderingUtils.ReAllocateIfNeeded(ref _sourceHandle, desc, name: "_TempColorTexture");
         }
 
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
@@ -57,19 +59,30 @@ public class OutlineLayerFeature : ScriptableRendererFeature
             {
                 new("SRPDefaultUnlit"),
                 new("UniversalForward"),
-                new("LightweightForward")
+                new("LightweightForward"),
+                new("Universal")
             };
+            var block = new RenderStateBlock(RenderStateMask.Nothing);
             var filteringSettings = new FilteringSettings(RenderQueueRange.opaque, LayerMask.GetMask(_layerName));
-            var drawSettings = CreateDrawingSettings(shaderTags, ref renderingData, renderingData.cameraData.defaultOpaqueSortFlags);
+            var drawSettings = CreateDrawingSettings(shaderTags, ref renderingData, renderingData.cameraData.defaultOpaqueSortFlags); 
             drawSettings.overrideMaterial = _replacementMaterial;
-            context.DrawRenderers(renderingData.cullResults, ref drawSettings, ref filteringSettings);
-
+            drawSettings.overrideMaterialPassIndex = 0;
+            
             var cmd = CommandBufferPool.Get();
             var source = renderingData.cameraData.renderer.cameraColorTargetHandle;
-            Blitter.BlitCameraTexture(cmd, source, _tmpHandle); 
+             
+            Blitter.BlitCameraTexture(cmd, source, _sourceHandle);            
+            CoreUtils.SetRenderTarget(cmd, _outlineHandle);
+            CoreUtils.ClearRenderTarget(cmd, ClearFlag.All, Color.black);
+            context.ExecuteCommandBuffer(cmd);
+            cmd.Clear();
+            
+            context.DrawRenderers(renderingData.cullResults, ref drawSettings, ref filteringSettings, ref block);
+
 
             CoreUtils.SetRenderTarget(cmd, source);
-            _outlineMaterial.SetTexture(Shader.PropertyToID("_BlitTexture"), _tmpHandle);
+            _outlineMaterial.SetTexture(Shader.PropertyToID("_OutlinedTexture"), _outlineHandle);
+            _outlineMaterial.SetTexture(Shader.PropertyToID("_BlitTexture"), _sourceHandle);
             CoreUtils.DrawFullScreen(cmd, _outlineMaterial);
 
             context.ExecuteCommandBuffer(cmd);
@@ -78,7 +91,7 @@ public class OutlineLayerFeature : ScriptableRendererFeature
         
         public void Dispose()
         {
-            _tmpHandle?.Release();
+            _outlineHandle?.Release();
         }
     }
 }
